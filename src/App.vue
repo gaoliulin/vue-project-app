c13f3a200cc400cbd6fdaac355daaaa01c33c7df
gaoliulin <410157812@qq.com>
2023-03-06 01:20:29 +0800

    引入 element ui & 设计布局1

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -18,6 +17,8 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+
+
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
+import Main from './Main.vue'
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(Main)
 
 app.use(createPinia())
 app.use(router)
+// 全局配置 ElementPlus
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
 app.mount('#app')
